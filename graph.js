ca5c590d53ba14bd73d5038f937e3204c30cd204
Palle Cogburn <palle@4thex.com>
2020-07-21 20:06:58 -0400

    Added axes. Not working yet. Using d3.scale*.

--- graph.js
+++ graph.js
@@ -4,7 +4,7 @@ let Graph = data => {
     .append("svg")
     .attr("width", 500)
     .attr("height", 500);
-  let domain = [
+  let domainX = [
     data.reduce((acc, item) => {
       return Math.min(acc, item.time);
     }, new Date(1E15)),
@@ -12,12 +12,29 @@ let Graph = data => {
       return Math.max(acc, item.time);
     }, new Date(-1E15))
   ];
-  let scale = d3.scaleLinear()
-    .domain(domain)
+  let scaleX = d3.scaleTime()
+    .domain(domainX)
     .range([0, 500]);
   let x_axis = d3.axisBottom()
-    .scale(scale);
+    .scale(scaleX);
   svg.append("g")
-         .call(x_axis);
+    .call(x_axis);
+
+  let domainY = [
+    data.reduce((acc, item) => {
+      return Math.min(acc, item.elev);
+    }, 1E5),
+    data.reduce((acc, item) => {
+      return Math.max(acc, item.elev);
+    }, -1E5)
+  ];
+  let scaleY = d3.scaleLinear()
+    .domain(domainY)
+    .range([0, 500]);
+  let y_axis = d3.axisLeft()
+    .scale(scaleY);
+  svg.append("g")
+    .call(y_axis);
+
 };
 export { Graph };
